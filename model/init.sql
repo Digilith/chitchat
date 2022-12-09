2a4262a1698b4d154e729cc464a7bada19eafb14
Elina <eliboc@inbox.ru>
2022-12-09 09:14:31 +0300

    filled the db

--- model/init.sql
+++ model/init.sql
@@ -58,9 +58,33 @@ VALUES ("test@mail.ru", "test", "1234"),
        ("test_2@mail.ru", "test_2", "1234"),
        ("test_3@mail.ru", "test_3", "1234");
 
-# TODO: add dummy info, 1) room id 1 GENERAL, user id 1 ADMIN 2) other dummy users, rooms, requests
+INSERT INTO room (admin_id, room_name, room_desc)
+VALUES (1, "general", "A chatroom for everything."),
+       (1, "gardening", "A chatroom for everyone interested in gardening."),
+       (1, "books", "Are you an avid reader? Join the discussion!"),
+       (2, "movies", "Anyone interested in movies and cinema are welcome! Make sure to write a little bit about yourself before applying!");
+
+INSERT INTO request (person_id, room_id, request_desc, status)
+VALUES (1, 4, "I like soap operas and romcoms, can I join?", 0),
+       (3, 4, "Hello, I'm a beginner director, would love to chat!", 0),
+       (2, 2, "Hi, I'd like to join", 0);
+
+INSERT INTO person_room(person_id, room_id)
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (1, 2),
+       (3, 2);
+
+INSERT INTO message(person_id, room_id, message_txt)
+VALUES (1, 1, "Hello, World!"),
+       (2, 1, "Hi everyone!"),
+       (3, 1, "Hi, how's it going?"),
+       (1, 1, "OK, you?"),
+       (3, 1, "Nothing special really");
+
+
 # TODO: TRIGGER RULE upon adding a user add them to general
-# TODO: fill out person_room table
 # TODO: TRIGGER RULE upon creating a room add the admin to person_room table
 # TODO: TRIGGER RULE upon deleting a room delete all person_room records and requests w/ it
-# TODO: TRIGGER RULE upon deleting a user delete the ROOM anf then all the records with them in all the tables
+# TODO: TRIGGER RULE upon deleting a user delete the ROOM and then all the records with them in all the tables
